61a95d311c6e0b5a5d91ef309c390eb8a24cb8a9
home <8she682GSYhaas@protonmail.org>
2024-01-21 12:50:10 -0700

    updated esoterica css

--- app/esoterica/page.js
+++ app/esoterica/page.js
@@ -1,14 +1,16 @@
 "use client"
 import React, { useState } from 'react';
 import Image from 'next/image';
+import styles from './page.module.css'
 
 const Carousel = () => {
 
   const numImages = 226; // Replace with the actual number of images you have
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(Math.floor(Math.random() * numImages) + 1);
 
+  
   const getImageName = (index) => `Photo${(index + 1).toString().padStart(3, '0')}.jpg`;
-
+  
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % numImages);
   };
@@ -16,24 +18,24 @@ const Carousel = () => {
   const prevSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + numImages) % numImages);
   };
-
+  
   const randomSlide = () => {
     setCurrentIndex(Math.floor(Math.random() * numImages) + 1)
   }
 
   return (
-    <div className="carousel-container">
-      <div className="carousel">
+    <div className={styles.carousel_container}>
+      <div className={styles.carousel}>
         {Array.from({ length: numImages }).map((_, index) => (
           <div
             key={index}
-            className={`carousel-item ${index === currentIndex ? 'active' : 'notActive'}`}
+            className={`${styles.carousel_item} ${index === currentIndex ? `${styles.active}` : `${styles.notActive}`}`}
           >
             {
               index === currentIndex &&
               
               <Image
-                className={`image`}
+                className={styles.image}
                 src={`/images/esoterica/${getImageName(index)}`} // Assuming images are in the 'public/images/esoterica' directory
                 alt={`Photo ${index + 1}`}
                 width={300}
@@ -43,15 +45,15 @@ const Carousel = () => {
           </div>
         ))}
       </div>
-      <div className="carousel-buttons">
-        <button className="carousel-button" onClick={prevSlide}>
+      <div className={styles.carousel_buttons}>
+        <button className={styles.carousel_button} onClick={prevSlide}>
           Previous
         </button>
-        <button className="carousel-button" onClick={nextSlide}>
-          Next
-        </button>
-        <button className="carousel-button" onClick={randomSlide}>
+        <button className={`${styles.carousel_button} ${styles.random}`} onClick={randomSlide}>
           Random
+        </button>
+        <button className={styles.carousel_button} onClick={nextSlide}>
+          Next
         </button>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <div className={styles.item}>
-      <h1 className={styles.title}>
-        Tsion
-      </h1>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Tsion
+        </h1>
+      </div>
+      <div className={styles.item}>
+      </div>
     </div>
-    <div className={styles.item}>
-    </div>
-  </div>
 
   )
 }
